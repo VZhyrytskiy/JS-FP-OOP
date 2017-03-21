ee594751175a6589cf4d15c70d12098a1674e9c0
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-03-21 11:30:25 +0200

    convert tabs to spaces

--- 03_Singleton/10_Singleton/10_addSingletonGetter.js
+++ 03_Singleton/10_Singleton/10_addSingletonGetter.js
@@ -1,11 +1,11 @@
 // addSingletonGeteter from Google Closure Library (only idea)
 
 let addSingletonGeteter = function( constructor ) {
-	constructor.getInstance = function() {
-		if ( !constructor.instance_ ) {
-			constructor.instance_ = new constructor();
-		}
+    constructor.getInstance = function() {
+        if ( !constructor.instance_ ) {
+            constructor.instance_ = new constructor();
+        }
 
-		return constructor.instance_;
-	};
+        return constructor.instance_;
+    };
 };
